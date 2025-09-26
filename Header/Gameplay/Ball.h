4857985328a0c5760ddd7f8834359f75b9ac09cb
Emirhan Topal <emirhantopal03@hotmail.com>
2025-09-26 18:55:26 +0300

    sprite and texture added for ball
    not only drawing a circle we use texture for ball

--- Header/Gameplay/Ball.h
+++ Header/Gameplay/Ball.h
@@ -2,16 +2,28 @@
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+using namespace std;
 
 namespace Gameplay_N
 {
 	class Ball
 	{
 		private:
-			CircleShape ball_sprite;
+			/*CircleShape ball_sprite;
 			const float ball_radius = 10.0f;
 			const float ball_pos_x = 500.0f;
-			const float ball_pos_y = 500.0f;
+			const float ball_pos_y = 500.0f;*/
+			Texture ball_texture;
+			Sprite ball_sprite;
+			string ball_texture_path = "Assets/Textures/Ball.png";
+
+			const float ball_scale_x = 0.06f;
+			const float ball_scale_y = 0.06f;
+			const float ball_pos_x = 615.0f;
+			const float ball_pos_y = 325.0f;
+
+			void loadTextures();
+			void initializeVariables();
 		public:
 			Ball();
 			void update(); // for ball movement 
--- Source/Gameplay/ball.cpp
+++ Source/Gameplay/ball.cpp
@@ -4,9 +4,11 @@ namespace Gameplay_N
 {
 	Ball::Ball()
 	{
-		ball_sprite.setRadius(ball_radius);
+		/*ball_sprite.setRadius(ball_radius);
 		ball_sprite.setPosition(ball_pos_x, ball_pos_y);
-		ball_sprite.setFillColor(Color::Blue);
+		ball_sprite.setFillColor(Color::Blue);*/
+		loadTextures();
+		initializeVariables();
 	}
 
 	void Ball::update()
@@ -17,5 +19,20 @@ namespace Gameplay_N
 	void Ball::render(RenderWindow *_game_window)
 	{
 		_game_window->draw(ball_sprite);
+	}
+
+	void Ball::loadTextures()
+	{
+		if (!ball_texture.loadFromFile(ball_texture_path))
+		{
+			throw std::runtime_error("Failed to laod ball texture!");
+		}
+	}
+
+	void Ball::initializeVariables()
+	{
+		ball_sprite.setTexture(ball_texture);
+		ball_sprite.setScale(ball_scale_x, ball_scale_y);
+		ball_sprite.setPosition(ball_pos_x, ball_pos_y);
 	}
 }
